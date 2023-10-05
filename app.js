a6e490e587038f1d09c02d2ad27c2e7518728c34
Yoonjieut <qmdldos@gmail.com>
2023-10-05 17:02:58 +0900

    fs 모듈로 메인 페이지 index.html 가져오기

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(function(request, response,){
   // console.log(response.url);
   // console.log(request.method);
   if(request.url === "/") {
-
+    fs.readFile('index.html','utf8',function(err, data){
+      if(err) {
+        console.error("에러가 발생했습니다.");
+      }
+      else {
+        console.log(data);
+      }
+    })
 
     response.writeHead(200, {"Content-Type" : "text/html"})
     response.end("main")
